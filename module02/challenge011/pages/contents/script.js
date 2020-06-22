9e4491ef45de374d8ee087e877cb0c734c56d1c7
William Oliveira <williamoliverpro@gmail.com>
2020-06-21 23:59:28 -0300

    challenge011

---
+++ module02/challenge011/pages/contents/script.js
@@ -0,0 +1,14 @@
+const modalOverlay = document.querySelector(".modal-overlay")
+const cards = document.querySelectorAll(".card")
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        const pageId = card.getAttribute("id")
+        modalOverlay.classList.add("active")
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${pageId}`
+    })
+}
+
+modalOverlay.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove("active")
+})
